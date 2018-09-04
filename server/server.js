3a010c7e126a2e89e325a5be7fe5773fd1e16926
Sarah Flynn <sarah.flynn@icloud.com>
2018-09-04 13:59:57 -0700

    installed packages, started server setup

---
+++ server/server.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const request = require('superagent');
+
+const cors= require('cors');
+const morgan = require('morgan');
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static('public'));
+
+const client = require('./db-client');
+
+app.post('/api/auth/signup', (req, res) => {
+  const body = req.body;
+  const email = body.email;
+  const password = body.password;
+
+  if(!email || !password) {
+    res.status(400).send({
+      error: 'email and password are required'
+    });
+    return
+  }
+
+  client.query(`
+    select cout(*)
+    from users
+    where email = $1
+  `,
+  [email])
+    .then(results => {
+      if(results.rows[0].count > 0) {
+        res.status(400).send({ error: 'email already in use' });
+        return;
+      }
+
+      client.query(`
+        insert into users (email, password)
+        values ($1, $2)
+        returning id, email
+      `,
+      [email, password])
+        .then(results => {
+          res.send(results.rows[0]);
+        });
+    });
+});
