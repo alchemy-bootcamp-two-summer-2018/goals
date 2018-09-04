f6c7c6c8a10d2063210eaa31e2d97ee803bf885e
Sarah Flynn <sarah.flynn@icloud.com>
2018-09-04 16:35:56 -0700

    App.vue finished

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,19 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>Goals</h1>
+
+      <nav>
+        <RouterLink to="/">Go to <strong>Home</strong></RouterLink>
+        &nbsp;
+        <RouterLink to="/goals">Go to Goals</RouterLink>
+        &nbsp;
+        <RouterLink v-if="!user" to="/auth">Sign In</RouterLink>
+        &nbsp;
+        <a v-if="user" href="/" @click.prevent="handleSignOut">Sign Out</a>
+      </nav>
+
+      <span v-if="user">user: {{ user.email }}</span>
+    </header>
+
+    <RouterView :onUser="handleUser"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { checkForToken, signOut } from './services.api';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      user: null
+    };
+  },
+  created() {
+    this.user = checkForToken();
+  },
+  methods: {
+    handleUser(user) {
+      this.user = user;
+    },
+    handleSignOut() {
+      signOut();
+      this.user = null;
+      this.$router.push('/');
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -21,8 +51,11 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1, nav {
+  text-align: center;
 }
 </style>
