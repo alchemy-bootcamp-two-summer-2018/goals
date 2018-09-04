23047cc25b13bb8105976e97d1c3f8aee20a7bac
MikeBLambert <mlamb128@gmail.com>
2018-09-04 16:34:31 -0700

    Populate Goals component

---
+++ app/src/components/Goals.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <ul>
+            <li>Hello World</li>
+        </ul>
+        <AddGoals :onEdit="onAdd"/>
+    </div>
+</template>
+
+<script>
+import AddGoals from './AddGoals.vue';
+
+export default {
+    props: ['onAdd'],
+    components: {
+        AddGoals
+    }
+
+}
+</script>
+
+<style>
+
+</style>
