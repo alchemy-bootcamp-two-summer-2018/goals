259449ec9fdc2be07ae91b006a2d1525fcd8ec6b
MikeBLambert <mlamb128@gmail.com>
2018-09-04 16:35:26 -0700

    populate drop-tables

--- server/scripts/drop-tables.js
+++ server/scripts/drop-tables.js
@@ -1,2 +1,14 @@
 require('dotenv').config();
 const client = require('../db-client');
+
+client.query(`
+    DROP TABLE IF EXISTS goals;
+    DROP TABLE IF EXISTS users;
+`)
+  .then(
+    () => console.log('Boom! Drop tables complete'),
+    err => console.log(err)
+  )
+  .then(() => {
+    client.end();
+  });
