07d80fee2c1a54f7d95dcf4902d5cd1ae3ab8130
Jack <ajackofsometrades@gmail.com>
2018-09-04 14:58:15 -0700

    setting up standard components

---
+++ app/src/components/Header.vue
@@ -0,0 +1,80 @@
+<template>
+    <header>
+    <img alt="Bacon" src="../assets/logo.png">
+    
+    <div>
+      <h1>Bacon</h1>
+      <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/auth">Sign Up</router-link>
+        <router-link to="/goals">Goals</router-link>
+        <router-link to="/users">Users</router-link>
+      </nav>
+    </div>
+
+
+  </header>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+
+img {
+  width: 200px;
+  height: 200px;
+}
+
+a {
+  display: inline-block;
+  width: 140px;
+  font-size: 24px;
+  padding: 10px;
+  margin: 15px;
+  border: 1px solid black;
+  text-decoration: none;
+  color: white;
+  border-radius: 5px;
+  box-shadow: 0.5px 0.5px 0.5px black;
+  background-color: rgba(65,214,195,0);
+  border: 4px solid #ffd90000;
+}
+
+a:hover {
+  background-color: rgba(65,214,195,1);
+  box-shadow: 0.9px 0.9px 0.9px black;
+  border: 4px solid rgba(243, 221, 97, 0.582);
+}
+
+h1 {
+  font-family: 'Baloo Tammudu', cursive;
+  font-size: 74px;
+  font-weight: normal;
+  display: inline-block;
+  height: 100px;
+  margin: 5px 0px 0px 0px;
+  text-transform: uppercase;
+  color: white;
+  text-shadow: 2px 2px black;
+}
+
+nav {
+  padding-bottom: 20px;
+}
+
+header {
+  display: grid;
+  grid-template-columns: 25% 50% 25%;
+  box-shadow: 3px 2px 2px black;
+  background: rgb(90,170,250);
+}
+
+header:hover {
+  background: rgba(65,214,195,1);
+}
+
+</style>
---
+++ app/src/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <main>
+    <h2>Can you bring home the bacon?</h2>
+    <p>
+      Here at bacon, we think goal setting matters. We want to help people achieve what they desire, which is bringing home the bacon.
+    </p>
+    <p class="center">
+      <img src="../assets/bacon.gif" />>
+    </p>
+  </main>
+</template>
+
+<script>
+
+
+
+export default {
+
+  data() {
+    return {
+      proof: `
+        
+      `
+    };
+  }
+
+
+};
+</script>
+
+<style scoped>
+
+h2 {
+  font-size: 36px;
+}
+
+p.center {
+  text-align: center;
+}
+
+p {
+  font-size: 18px;
+  text-align: left;
+  width: 30%;
+  padding: 10px;
+  margin: 10px auto;
+}
+
+image {
+  width: 700px;
+}
+
+</style>
