0a5ef12d5670f1f2040e73be4fb9010bfaded401
MikeBLambert <mlamb128@gmail.com>
2018-09-04 16:35:16 -0700

    populate create-tables

--- server/scripts/create-tables.js
+++ server/scripts/create-tables.js
@@ -1,2 +1,23 @@
 require('dotenv').config();
 const client = require('../db-client');
+
+client.query(`
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(256) NOT NULL,
+        password VARCHAR(256) NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS goals (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(256),
+        user_id INTEGER NOT NULL REFERENCES users(id)
+    );
+`)
+    .then(
+        () => console.log('Booya! Create tables complete'),
+        err => console.log(err)
+    )
+    .then(() => {
+        client.end();
+    })
