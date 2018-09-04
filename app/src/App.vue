2889733ed9b5ca7c790e78a5ab40d6ae8f009aa1
Jack <ajackofsometrades@gmail.com>
2018-09-04 14:58:30 -0700

    setting up app.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <router-view></router-view>
+    <Footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import api from './services/api';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
-}
+    Header, Footer
+  },
+
+
+
+};
 </script>
 
-<style scoped>
+<style>
 
 #app {
   display: grid;
