e2ed735afc43aace1e49eabf00b6eb9e9fb5ccbc
Sarah Flynn <sarah.flynn@icloud.com>
2018-09-04 16:58:34 -0700

    Auth.vue template done

---
+++ app/src/components/Auth.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2>{{ label }}</h2>
+    <button @click="type = isSignUp ? 'signIn' : 'signUp'">
+      {{
+        isSignUp
+          ? "Already a user? Click here to sign in"
+          : "New? Click here to sign up"
+      }}
+    </button>
+    <pre>{{ error }}</pre>
+    <form @submit.prevent="handleSubmit">
+        <FormControl label="email">
+          <input v-model="credentials.email">
+        </FormControl>
+        <FormControl label="password">
+          <input
+            :type="show ? 'text' : 'password'"
+            v-model="credentials.password">
+          <button 
+            @click="show = !show"
+            type="button"
+          >
+            {{ show ? 'Hide' : 'Show' }}
+          </button>
+        </FormControl>
+        <FormControl>
+          <button type="submit">
+            {{ label }}
+          </button>
+        </FormControl>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
