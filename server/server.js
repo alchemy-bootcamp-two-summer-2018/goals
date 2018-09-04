e12098f4c29a25ed7ca52bc5ecb19025f2e29b6c
Jack <ajackofsometrades@gmail.com>
2018-09-04 14:18:14 -0700

    callling .env in server.js and db-client

--- server/server.js
+++ server/server.js
@@ -20,110 +20,239 @@ app.use(express.static('public'));
 // connect to database
 const client = require('./db-client');
 
-// routes
-app.get('/api/cities', (req, res) => {
-  client.query(`
-    SELECT
-      c.id,
-      c.name,
-      s.id as "stateId",
-      s.name as state
-    FROM cities as c
-    JOIN states as s
-    ON c.state_id = s.id
-    ORDER BY c.name;
-  `)
-    .then(result => {
-      res.send(result.rows);
-    })
-    .catch(err => console.log(err));
-});
-
-app.get('/api/cities/:id', (req, res) => {
-  client.query(`
-    SELECT
-      id,
-      name,
-      state_id as "stateId",
-      population,
-      landlocked
-    FROM cities
-    WHERE id = $1;
-  `,
-  [req.params.id]
-  )
-    .then(result => {
-      res.send(result.rows[0]);
-    })
-    .catch(err => console.log(err));
-});
-
-app.post('/api/cities', (req, res) => {
-  console.log('posting');
+// auth routes
+app.post('/api/auth/signup', (req, res) => {
   const body = req.body;
+  const email = body.email;
+  const password = body.password;
+
+  if(!email || !password) {
+    res.status(400).send({
+      error: 'email and password are required'
+    });
+    return;
+  }
 
   client.query(`
-    INSERT INTO cities (name, state_id, population, landlocked)
-    VALUES ($1, $2, $3, $4)
-    RETURNING *;
+    select count(*)
+    from users
+    where email = $1
   `,
-  [body.name, body.stateId, body.population, body.landlocked]
-  )
-    .then(result => {
-      res.send(result.rows[0]);
-    })
-    .catch(err => console.log(err));
+  [email])
+    .then(results => {
+      if(results.rows[0].count > 0) {
+        res.status(400).send({ error: 'email already in use' });
+        return;
+      }
+
+      client.query(`
+        insert into users (email, password)
+        values ($1, $2)
+        returning id, email
+      `,
+      [email, password])
+        .then(results => {
+          res.send(results.rows[0]);
+        });
+    });
+
 });
 
-app.put('/api/cities/:id', (req, res) => {
-  console.log('updating');
+app.post('/api/auth/signin', (req, res) => {
   const body = req.body;
+  const email = body.email;
+  const password = body.password;
+
+  if(!email || !password) {
+    res.status(400).send({
+      error: 'email and password are required'
+    });
+    return;
+  }
 
   client.query(`
-    UPDATE cities 
-    SET 
-      name=$1, 
-      state_id=$2, 
-      population=$3, 
-      landlocked=$4
-    WHERE id = $5
-    RETURNING *;
+    select id, email, password
+    from users
+    where email = $1
   `,
-  [body.name, body.stateId, body.population, body.landlocked, req.params.id]
+  [email]
   )
-    .then(result => {
-      res.send(result.rows[0]);
-    })
-    .catch(err => console.log(err));
-});
-
-app.delete('/api/cities/:id', (req, res) => {
-  console.log('deleting');
-
-  client.query(`
-    DELETE FROM cities 
-    WHERE id = $1;
-  `,
-  [req.params.id]
-  )
-    .then(() => {
-      res.send({ removed: true });
-    })
-    .catch(err => console.log(err));
-});
-
-app.get('/api/states', (req, res) => {
-  client.query(`
-    SELECT *
-    FROM states;
-  `)
-    .then(result => {
-      res.send(result.rows);
+    .then(results => {
+      const row = results.rows[0];
+      if(!row || row.password !== password) {
+        res.status(401).send({ error: 'invalid email or password' });
+        return;
+      }
+      res.send({ 
+        id: row.id,
+        email: row.email
+      });
     });
 });
+
+app.use((req, res, next) => {
+  // is there a Authorization header?
+  const id = req.get('Authorization');
+  if(!id) {
+    // no - send an error
+    res.status(403).send({
+      error: 'No token found'
+    });
+    return;
+  }
+
+  // 1. set req.userId to the header
+  req.userId = id;
+  // 2. call next()
+  next();
+});
+
+// api data routes
+app.get('/api/neighborhoods', (req, res, next) => {
+
+  client.query(`
+    select id, 
+      name, 
+      quadrant_id as "quadrantId", 
+      description, 
+      population, 
+      founded
+    from neighborhoods
+    order by name;
+  `).then(result => {
+    res.send(result.rows);
+  })
+    .catch(next);
+
+});
+
+app.post('/api/neighborhoods', (req, res, next) => {
+  const body = req.body;
+  if(body.name === 'error') return next('bad name');
+  
+  client.query(`
+    insert into neighborhoods (user_id, name, quadrant_id, population, founded, description)
+    values ($1, $2, $3, $4, $5, $6)
+    returning *, quadrant_id as "quadrantId";
+  `,
+  [req.userId, body.name, body.quadrantId, body.population, body.founded, body.description]
+  ).then(result => {
+    // send back object
+    res.send(result.rows[0]);
+  })
+    .catch(next);
+});
+
+app.put('/api/neighborhoods/:id', (req, res, next) => {
+  const body = req.body;
+
+  client.query(`
+    update neighborhoods
+    set
+      name = $1,
+      quadrant_id = $2,
+      population = $3,
+      founded = $4,
+      description = $5
+    where id = $6,
+    and user_id = $7
+    returning *, quadrant_id as "quadrantId";
+  `,
+  [body.name, body.quadrantId, body.population, body.founded, 
+    body.description, req.params.id, req.userId]
+  ).then(result => {
+    res.send(result.rows[0]);
+  })
+    .catch(next);
+});
+
+app.delete('/api/neighborhoods/:id', (req, res, next) => {
+  client.query(`
+    delete from neighborhoods where id=$1;
+  `,
+  [req.params.id]
+  ).then(() => {
+    res.send({ removed: true });
+  })
+    .catch(next);
+});
+
+app.get('/api/quadrants', (req, res, next) => {
+
+  client.query(`
+    select 
+      q.id, q.name, q.direction,
+      count(n.id) as "neighborhoodCount",
+      avg(n.population) as "populationAvg "
+    from quadrants q
+    left join neighborhoods n
+    on q.id = n.quadrant_id
+    group by q.id
+    order by q.name;
+  `)
+    .then(result => {
+      res.send(result.rows);
+    })
+    // we don't need the wrapper function:
+    // .catch(err => {
+    //   next(err);
+    // });
+    // we can just pass next _as_ the error callback function:
+    .catch(next);
+});
+
+app.get('/api/quadrants/:id', (req, res, next) => {
+
+  const quadrantPromise = client.query(`
+    select id, name, direction
+    from quadrants q
+    where q.id = $1;
+  `,
+  [req.params.id]);
+
+  const neighborhoodsPromise = client.query(`
+    select id, name, description
+    from neighborhoods
+    where quadrant_id = $1;
+  `,
+  [req.params.id]);
+
+  Promise.all([quadrantPromise, neighborhoodsPromise])
+    .then(promiseValues => {
+      const quadrantResult = promiseValues[0];
+      const neighborhoodsResult = promiseValues[1];
+
+      if(quadrantResult.rows.length === 0) {
+        res.sendStatus(404);
+        return;
+      }
+
+      const quadrant = quadrantResult.rows[0];
+      const neighborhoods = neighborhoodsResult.rows;
+      quadrant.neighborhoods = neighborhoods;
+
+      res.send(quadrant);
+    })
+    .catch(next);
+});
+
+app.get('/api/restaurants', (req, res, next) => {
+  request.get(`${process.env.RESTAURANTS_API}/restaurant-inspections/`)
+    .then(result => {
+      res.send(result.body.results.map(rest => {
+        return {
+          address: rest.address,
+          name: rest.name,
+          inspectionNumber: rest.inspection_number
+        };
+      }));
+    })
+    .catch(next);
+});
+
 
 
 
 // start "listening" (run) the app (server)
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log('app running on', PORT));
