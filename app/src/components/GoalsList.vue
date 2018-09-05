1173e6cd10fcb80f84e370f4ba8c3a55a465b7dd
Sarah Flynn <sarah.flynn@icloud.com>
2018-09-04 18:01:05 -0700

    GoalsList.vue done

---
+++ app/src/components/GoalsList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h3>Goals List</h3>
+    <div v-if="goals">
+      <p
+        v-for="goal in goals"
+        :key="goal.id"
+      >
+        {{ goal.goal }}
+        {{ goal.status }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['goals']
+};
+
+</script>
+
+<style>
+
+</style>
