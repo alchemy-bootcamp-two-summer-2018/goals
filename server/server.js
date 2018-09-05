19cce5083914e41bd1ac58e1f4a74fb028d36f12
Alex Rankin <alex.c.rankin@gmail.com>
2018-09-04 17:25:48 -0700

    added user is making it to DB

--- server/server.js
+++ server/server.js
@@ -22,4 +22,34 @@ app.post('/api/auth/signup', (req, res) => {
   const body = req.body;
   const email = body.email;
   const password = body.password;
+
+  if(!email || !password) {
+    res.status(400).send({
+      error: 'Please enter an email and password.'
+    });
+    return;
+  }
+
+  client.query(`
+    SELECT count(*)
+    FROM users
+    WHERE email = $1
+  `,
+  [email])
+    .then(results => {
+      if(results.rows[0].count > 0) {
+        res.status(400).send({ error: 'already a user with this address' });
+        return;
+      }
+    
+      client.query(`
+        INSERT INTO users (email, password)
+        VALUES ($1, $2)
+        RETURNING id, email
+      `,
+      [email, password])
+        .then(results => {
+          res.send(results.rows[0]);
+        });    
+    });
 });
