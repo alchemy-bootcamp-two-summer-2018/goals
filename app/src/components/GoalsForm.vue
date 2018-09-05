6d53f761677f34932a83bb3c8ca328b18f2a02c9
Sarah Flynn <sarah.flynn@icloud.com>
2018-09-04 17:55:31 -0700

    basic GoalForm.vue done

--- app/src/components/GoalsForm.vue
+++ app/src/components/GoalsForm.vue
@@ -21,9 +21,46 @@
 </template>
 
 <script>
+import FormControl from './FormControl';
+
+const initGoal = () => {
+  return {
+    goal: '',
+    status: ''
+  };
+};
+
 export default {
-  props: {
-    label: String
+  components: {
+    FormControl
+  },
+  props : {
+    goal: Object,
+    goals: Array,
+    label: String,
+    onEdit: {
+      type: Function,
+      required: true
+    },
+    onCancel: Function
+  },
+  data() {
+    return {
+      error: null,
+      edit: this.goal ? Object.assign({}, this.goal) : initGoal()
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.error = null;
+      return this.onEdit(this.edit)
+        .then (() => {
+          this.edit = initGoal();
+        })
+        .catch(err => {
+          this.error = err;
+        });
+    }
   }
 };
 </script>
