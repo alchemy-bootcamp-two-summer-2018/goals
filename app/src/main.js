22537c77b4edac034f0124947f19fa2d995ddac1
Jack <ajackofsometrades@gmail.com>
2018-09-04 18:22:31 -0700

    adding tabs css to main js

--- app/src/main.js
+++ app/src/main.js
@@ -3,9 +3,13 @@ import VueRouter from 'vue-router';
 import App from './App.vue';
 import router from './router';
 import './main.css';
+import './tabs.css';
+
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
+Vue.component('RouterLink', Vue.component('router-link'));
+Vue.component('RouterView', Vue.component('router-view'));
 
 new Vue({
   router,
