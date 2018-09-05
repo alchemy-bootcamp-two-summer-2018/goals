bd7dd2f636d5d02df623b9c3952792d004d0af7a
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-09-05 12:26:44 -0700

    meh

--- app/src/components/User.vue
+++ app/src/components/User.vue
@@ -18,33 +18,5 @@ export default {
 </script>
 
 <style scoped>
-/* li {
-  list-style: none;
-  text-align: center;
-  position: relative;
-}
-h3{
-  font-family:'Playfair Display';
-  font-size:25px;
-  letter-spacing: 1px;
-  color:white;
-  margin-bottom:0;
-  text-transform: none;
-}
-p {
-  margin-top: 15px;
-  font-size: 10px;
-  text-transform: uppercase;
-  letter-spacing: 2px;
-  text-align: center !important;
-}
-.title-bg {
-  background: black;
-  color:white;
-  position: absolute;     
-  text-align: center;  
-  padding: 0px 20px 10px 20px;  
-  bottom: 0px; 
-  width: 100%;
-} */
+
 </style>
