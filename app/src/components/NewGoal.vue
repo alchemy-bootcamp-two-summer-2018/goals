9e4cba7416dbdb833631e6ceef1844b9d6ba46a8
Sarah Flynn <sarah.flynn@icloud.com>
2018-09-04 18:04:18 -0700

    NewGoal.vue done

---
+++ app/src/components/NewGoal.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <GoalForm
+      label="Add"
+      :on-edit="onAdd"/>
+  </section>
+</template>
+
+<script>
+import GoalForm from './GoalForm';
+
+export default {
+  props: ['onAdd'],
+  components: {
+    GoalForm
+  }
+};
+</script>
+
+<style>
+
+</style>
