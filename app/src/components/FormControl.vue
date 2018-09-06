126e0e2f5d21bfe218854bd8a298158c3801278f
Alex Rankin <alex.c.rankin@gmail.com>
2018-09-06 14:51:38 -0700

    rendering list of goals and their completed/non-completed status

--- app/src/components/FormControl.vue
+++ app/src/components/FormControl.vue
@@ -20,4 +20,5 @@ export default {
 
 <style>
 
+
 </style>
--- app/src/components/Goal.vue
+++ app/src/components/Goal.vue
@@ -1,16 +1,23 @@
 <template>
-  <li>
-    Each of these things is a goal
+  <li class="goal">
+    <h3>{{ goal. name }}</h3>
+    <p v-if="goal.completed">Completed</p>
+    <p v-else>Not completed</p>
   </li>
     
 </template>
 
 <script>
 export default {
-
+  props: {
+    goal: Object
+  }
 };
 </script>
 
 <style>
+.goal {
+  list-style: none;
+}
 
 </style>
