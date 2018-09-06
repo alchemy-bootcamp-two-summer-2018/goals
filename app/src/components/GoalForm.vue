6ef731ec56713461fb41d4647c4287720ac39d77
Claire Flanagan <claireflanagan@gmail.com>
2018-09-06 13:42:57 -0700

    working on form

--- app/src/components/GoalForm.vue
+++ app/src/components/GoalForm.vue
@@ -6,11 +6,30 @@
           name="name" placeholder="Name" required
           v-model="edit.name">
       </FormControl>
+      <FormControl label="Goal">
+        <input type="text" name="goal" placeholder="goal" required
+          v-model.number="edit.goal">
+      </FormControl>
+      <FormControl>
+        <button type="submit">{{ label }} </button>
+        <button
+          v-if="onCancel"
+          @click="onCancel">
+          Cancel
+        </button>
+      </FormControl>
     </form>
   </section>
 </template>
 
 <script>
+import FormControl from './FormControl';
+
+const initGoal = () => {
+  return {
+    name
+  }
+}
 export default {
 
 };
