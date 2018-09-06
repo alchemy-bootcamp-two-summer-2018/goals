4ed8b34d51f64455f3030e933720913bd4450003
MikeBLambert <mlamb128@gmail.com>
2018-09-06 15:51:09 -0700

    Add CSS styles for mobile

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -2,12 +2,31 @@
   
   <header>
     <h2>Goals goals goals!</h2>
+    <RouterLink class="nav-link" to="/">Home</RouterLink>
+    <RouterLink v-if="user" class="nav-link" to="/goals">Goals</RouterLink>
+    <RouterLink v-if="!user" class="nav-link" to="/auth">Sign In</RouterLink>
+    <a v-if="user" href="/" @click.prevent="handleSignOut">Sign Out</a>
   </header>
 </template>
 
 <script>
 export default {
   name: 'Header',
+  data() {
+    return {
+      user: null
+    };
+  },
+  methods: {
+    handleUser(user) {
+      this.user = user;
+    },
+    handleSignOut() {
+      signOut();
+      this.user = null;
+      this.$router.push('/');
+    }
+  },
 };
 </script>
 
