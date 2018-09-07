303158c92dc5a1cbbf9a7af3a31192ef638ec3ae
Sarah Flynn <sarah.flynn@icloud.com>
2018-09-07 09:32:01 -0700

    responsive header and form styling progress

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,27 +1,30 @@
 <template>
   <div id="app">
-    <header>
-        <nav>
-          <div class="links1">
-            <RouterLink class="nav-link" to="/">Home</RouterLink>
-            &nbsp;
-            <RouterLink class="nav-link" to="/goals">Goals</RouterLink>
-            &nbsp;
-          </div>
-          <div class="logo">
-            <img src="http://inspower.co/wp-content/uploads/2015/11/do-what-you-love-cg.jpg" alt="Do what you love. Love what you do.">
-          </div>
-          <div class="links2">
-            <RouterLink v-if="!user" to="/auth" class="nav-link">Sign In</RouterLink>
-            &nbsp;
-            <a v-if="user" class="nav-link" href="/" @click.prevent="handleSignOut">Sign Out</a>
-            <a class="nav-link">About</a>
-          </div>
-        </nav>
-      <span v-if="user">user: {{ user.email }}</span>
-    </header>
+    <div class="content">
+      <header>
+          <nav>
+            <div class="links1 nav">
+              <RouterLink class="nav-link" to="/">Home</RouterLink>
+              <a class="nav-link">About</a>
+            </div>
+            <div class="logo nav">
+              <img src="http://inspower.co/wp-content/uploads/2015/11/do-what-you-love-cg.jpg" alt="Do what you love. Love what you do.">
+            </div>
+            <div class="links2 nav">
+              <RouterLink class="nav-link" to="/goals">Goals</RouterLink>
+              <RouterLink v-if="!user" to="/auth" class="nav-link">Sign In</RouterLink>
+              <a v-if="user" class="nav-link" href="/" @click.prevent="handleSignOut">Sign Out</a>
+            </div>
+          </nav>
+        <span v-if="user">user: {{ user.email }}</span>
+      </header>
 
-    <RouterView :onUser="handleUser"/>
+      <RouterView :onUser="handleUser"/>
+
+    </div>
+    <footer>
+      <p>Created at Alchemy Code Lab</p>
+    </footer>
 
   </div>
 </template>
@@ -52,40 +55,95 @@ export default {
 </script>
 
 <style scoped>
-/* #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
 
-nav {
-  display: flex;
-  width: 100vw;
-  justify-content: space-around;
-}
+  .content {
+    min-height: 100vh;
+  }
 
-nav div {
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-}
+  .logo {
+    order: -1;
+  }
 
-nav img {
-  height: 300px;
-}
+  img {
+    width: 100vw;
+  }
 
-.links1,
-.links2 {
-  flex-grow: 2;
-}
+  nav {
+    display: flex;
+    width: 100vw;
+    flex-direction: column;
+  }
 
-.nav-link {
-  text-decoration: none;
-  color: black;
-  font-family: 'Amatic SC', sans-serif;
-  font-size: 40px;
+  .links1,
+  .links2 {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    height: fit-content;
+  }
+
+  .nav-link {
+    display: block;
+    width: 100vw;
+    line-height: 75px;
+    margin: 0;
+    padding: 0;
+  }
+
+  footer {
+    height: 70px;
+    margin-top: -75px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(255, 217, 0, 0.596);
+  }
+
+@media (min-width: 500px) {
+
+  .logo {
+    order: initial;
+  }
+
+  nav {
+    display: flex;
+    width: 100vw;
+    justify-content: space-around;
+    flex-direction: row;
+    border-bottom: 4px solid rgba(255, 217, 0, 0.596);
+    border-top: 4px solid rgba(255, 217, 0, 0.596);
+    padding: 10px;
+  }
+  nav div {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  .logo {
+    flex-grow: 1;
+    max-width: 20vw;
+  }
+
+  img {
+    width: initial;
+    max-width: 100%;
+    max-height: 100%;
+  }
+
+  .links1,
+  .links2 {
+    flex-grow: 2;
+    flex-direction: row;
+    height: initial;
+  }
+  .nav-link {
+    text-decoration: none;
+    color: black;
+    font-family: 'Amatic SC', sans-serif;
+    font-size: 40px;
+    width: initial;
+  }
 }
 
 </style>
--- app/src/components/Auth.vue
+++ app/src/components/Auth.vue
@@ -1,35 +1,38 @@
 <template>
   <div>
-    <h2>{{ label }}</h2>
-    <button @click="type = isSignUp ? 'signIn' : 'signUp'">
-      {{
-        isSignUp
-          ? "Already a user? Click here to sign in"
-          : "New? Click here to sign up"
-      }}
-    </button>
-    <pre>{{ error }}</pre>
-    <form @submit.prevent="handleSubmit">
-        <FormControl label="email">
-          <input v-model="credentials.email">
-        </FormControl>
-        <FormControl label="password">
-          <input
-            :type="show ? 'text' : 'password'"
-            v-model="credentials.password">
-          <button 
-            @click="show = !show"
-            type="button"
-          >
-            {{ show ? 'Hide' : 'Show' }}
-          </button>
-        </FormControl>
-        <FormControl>
-          <button type="submit">
-            {{ label }}
-          </button>
-        </FormControl>
-    </form>
+    <div class="form">
+      <h2>{{ label }}</h2>
+      <button @click="type = isSignUp ? 'signIn' : 'signUp'">
+        {{
+          isSignUp
+            ? "Already a user? Click here to sign in"
+            : "New? Click here to sign up"
+        }}
+      </button>
+      <pre>{{ error }}</pre>
+      <form @submit.prevent="handleSubmit">
+          <FormControl label="email">
+            <input v-model="credentials.email">
+          </FormControl>
+          <FormControl label="password">
+            <input
+              :type="show ? 'text' : 'password'"
+              v-model="credentials.password">
+            <button
+              class="inline-button"
+              @click="show = !show"
+              type="button"
+            >
+              {{ show ? 'Hide' : 'Show' }}
+            </button>
+          </FormControl>
+          <FormControl>
+            <button type="submit">
+              {{ label }}
+            </button>
+          </FormControl>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -81,6 +84,17 @@ export default {
 
 </script>
 
-<style>
+<style scoped>
+
+  .form {
+    margin: 50px auto;
+    text-align: center;
+  }
+
+  h2 {
+    font-size: 2em;
+  }
+
+
 
 </style>
